3670e88fb7d71ef48cc88ae64e8455c03279dce9
mkt-narola <mkt@narola.email>
2023-07-25 11:27:02 +0530

    Load question data and submit answers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import QuestionnaireModal from './components/QuestionnaireModal'
+import QuestionnaireModal from "./components/QuestionnaireModal";
+import { Provider } from "react-redux";
+import store from "./lib/store";
 
-localStorage.setItem('user', 'example@gmail.com') // Set user here
+localStorage.setItem("user", "mkt@narola.email"); // Set user here
 
 function App() {
-  return (
-    <div className="App">
-      <QuestionnaireModal />
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <QuestionnaireModal />
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
--- src/lib/userSlice.js
+++ src/lib/userSlice.js
@@ -1,1 +1,58 @@
-// Setup user slice here
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import services from "../services/questuinaiire";
+const initialState = {
+    isModalOpen: false,
+    submitted: false,
+};
+
+export const submitAnswers = createAsyncThunk("services/create", async (data) => {
+    const res = await services.create(data);
+    console.log("res.status => ", res.status);
+    if (res.status === 201) {
+        localStorage.setItem("noShow", "noshow");
+        localStorage.setItem("id", res.data.id);
+        console.log("res => ", res);
+        return res.data;
+    }
+});
+export const getAnswers = createAsyncThunk("services/get", async (data) => {
+    const res = await services.get(data);
+    console.log("res.data => ", res.data);
+    if (res.status === 201) {
+        return res.data;
+    }
+});
+
+export const userSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+        handleModal: (state, action) => {
+            console.log("action => ", action);
+            return { ...state, isModalOpen: action.payload };
+        },
+    },
+    extraReducers: {
+        [submitAnswers.fulfilled]: (state, action) => {
+            alert("submitted");
+            return { ...state, submitted: true, isModalOpen: false };
+        },
+        [submitAnswers.rejected]: (state, action) => {
+            alert("error");
+            return { ...state, submitted: true, isModalOpen: true };
+        },
+        [getAnswers.fulfilled]: (state, action) => {
+            alert("User has already submitted answers.");
+            return { ...state, submitted: true, isModalOpen: false };
+        },
+        [getAnswers.rejected]: (state, action) => {
+            alert("error");
+            return { ...state, submitted: true, isModalOpen: true };
+        },
+    },
+});
+
+export const { handleModal } = userSlice.actions;
+
+// this is for configureStore
+export default userSlice.reducer;
